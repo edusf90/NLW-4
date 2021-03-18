21aa77b91bec714afd77a1d6b589bd5356de74f0
Eduardo Santana <edusf0@hotmail.com>
2021-03-18 17:47:59 -0300

    finalização da aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,11 @@
 import 'reflect-metadata';
 import express, { request, response } from 'express'
 import './database';
+import { router } from './routes';
 const app = express();
 
-/*
-Get > buscar
-post > Salvar
-put > alterar
-delete > deletar
-patch > alteração específica 
+app.use(express.json());
+app.use(router)
 
-*/
-
-app.get("/", (request, response) =>{
-    return response.json({message: "Hello World - NLW 04"})
-})
-
-app.post("/", (request, response) =>{
-    return response.json({message: "Dados salvos com sucesso!"})
-})
-
-app.listen(3333, () => console.log("Server is runnig!"));
+app.listen(3333, () => console.log("Server is runnig! Port: 3333"));
 
